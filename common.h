b974a64ae27412534d1797982f267d74fe100f8e
Gregorio Flores <floresgreg@hotmail.com>
2017-03-25 19:31:31 -0500

    Created Repo
    Doesn't work yet

---
+++ common.h
@@ -0,0 +1,14 @@
+/*
+ * File:        common.h
+ * Purpose:     File included by non-standard NXP drivers.
+ *              Do not use in your projects, use CMSIS files!
+ *
+ * Notes:
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define DATA_BUF_SIZE   128
+
+#endif /* _COMMON_H_ */
