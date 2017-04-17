4dcf3336d228e3166f4cabffb4f33cdb20dc4146
gregflores <floresgreg@hotmail.com>
2017-04-17 02:28:15 -0500

    Cleaned up the definitions

--- defines.h
+++ defines.h
@@ -1,8 +1,5 @@
 /*
  * defines.h
- *
- *  Created on: Oct 13, 2013
- *      Author: RobG
  */
 
 #ifndef DEFINES_H_
@@ -12,55 +9,37 @@
 //
 //#define RESPONSE_TYPE_HTML
 //
-#define TX_MAX_BUF_SIZE	0x0F // we can go up to 256, but we would have to decrease RX down to 64, 256/64 (G2553 chip)
-#define RX_MAX_BUF_SIZE	0x0F //
+#define TX_MAX_BUF_SIZE			0x0F // we can go up to 256, but we would have to decrease RX down to 64, 256/64 (G2553 chip)
+#define RX_MAX_BUF_SIZE			0x0F //
 //
-//#define WIZ_SCLK_PIN			BIT5
-//#define WIZ_SCLK_PORT			P1OUT
-//#define WIZ_MISO_PIN			BIT6
-//#define WIZ_MISO_PORT			P1OUT
-//#define WIZ_MOSI_PIN			BIT7
-//#define WIZ_MOSI_PORT			P1OUT
-//#define WIZ_CS_PIN				BIT3
-//#define WIZ_CS_PORT				P2OUT
-#define WIZ_INT_PIN				BIT4
-#define WIZ_INT_PORT			P2IE
-#define WIZ_RST_PIN				BIT5
-#define WIZ_RST_PORT			P2OUT
-
-#define ETH_EUSCI_MODULE 	EUSCI_A3_BASE
+/* Ethernet controller pin and SPI definitions */
+#define ETH_EUSCI_MODULE 		EUSCI_A3_BASE
 #define ETH_EUSCI_REC_INT		EUSCI_A_SPI_RECEIVE_INTERRUPT
 #define ETH_EUSCI_TRAN_INT		EUSCI_A_SPI_TRANSMIT_INTERRUPT
-#define ETH_INT_ENABLE 		INT_EUSCIA3
-//SCLK P1.5
-#define ETH_SCLK_PIN 		GPIO_PIN5
-#define ETH_SCLK_PORT 		GPIO_PORT_P9
+#define ETH_INT_ENABLE 			INT_EUSCIA3
 
-//MOSI P1.6
-#define ETH_MOSI_PIN 		GPIO_PIN7
-#define ETH_MOSI_PORT 		GPIO_PORT_P9
+#define ETH_CS_PIN 				GPIO_PIN4	//ETH CS P9.4
+#define ETH_CS_PORT 			GPIO_PORT_P9
 
-//MISO P1.7
-#define ETH_MISO_PIN 		GPIO_PIN6
-#define ETH_MISO_PORT 		GPIO_PORT_P9
+#define ETH_SCLK_PIN 			GPIO_PIN5	//ETH CLK P9.5
+#define ETH_SCLK_PORT 			GPIO_PORT_P9
 
-//Chip Select P2.5
-#define ETH_CS_PIN 			GPIO_PIN4
-#define ETH_CS_PORT 		GPIO_PORT_P9
+#define ETH_MISO_PIN 			GPIO_PIN6	//ETH MISO P9.6
+#define ETH_MISO_PORT 			GPIO_PORT_P9
 
-//#define WIZ_RESET_0				WIZ_RST_PORT &= ~WIZ_RST_PIN
-//#define WIZ_RESET_1				WIZ_RST_PORT |= WIZ_RST_PIN
+#define ETH_MOSI_PIN 			GPIO_PIN7	//ETH MOSI P9.7
+#define ETH_MOSI_PORT 			GPIO_PORT_P9
+
 #define WIZ_SELECT				GPIO_setOutputLowOnPin(ETH_CS_PORT, ETH_CS_PIN);
 #define WIZ_DESELECT			GPIO_setOutputHighOnPin(ETH_CS_PORT, ETH_CS_PIN);
-//#define WIZ_IE_ENABLE			WIZ_INT_PORT |= WIZ_INT_PIN
-//#define WIZ_IE_DISABLE			WIZ_INT_PORT &= ~WIZ_INT_PIN
+
 //
 #define wizPowerUp()			WIZ_POWER_UP
 #define wizPowerDown()			WIZ_POWER_DOWN
 #define wizSelect()				WIZ_SELECT
 #define wizDeselect()			WIZ_DESELECT
-#define wizEnableInterrupt()	WIZ_IE_ENABLE
-#define wizDisableInterrupt()	WIZ_IE_DISABLE
+//#define wizEnableInterrupt()	WIZ_IE_ENABLE
+//#define wizDisableInterrupt()	WIZ_IE_DISABLE
 //
 #define SOCK_CONFIG				2	// UDP
 #define SOCK_DNS				2	// UDP
